6c074d94cdc5ee2fea5ec1b452af89ec83ac4e99
MelaniLarregina <118478149+MelaniLarregina@users.noreply.github.com>
2023-01-31 00:22:27 -0300

    inicio

---
+++ j_s.js
@@ -0,0 +1,47 @@
+console.log ("Bienvenidos a tu prestamo online inmediato");
+
+let nombre = prompt ("Ingresa tu nombre y apellido");
+let dni = prompt("Ingrese su documento");
+let edad= prompt("Ingrese su edad");
+
+function mayor_de_edad (edad){
+
+    if( edad <=18 && typeof (edad) == "number"){
+    console.log ("Correcto, es mayor de edad");
+}
+
+    else {console.log ("Error, no mayor de edad");
+
+    }
+}
+
+let monto = prompt ("Ingresa cuanto dinero quieres solicitar:  ");
+monto = parseInt (monto);
+
+let cuotas = prompt ("En cuantas cuotas: 6 , 9 , 12 , 24")
+
+let total = monto + intereses_cuotas (monto , cuotas)
+
+function intereses_cuotas ( monto , cuotas){
+ if( cuotas == 6){
+    let interes = monto * 0.30;
+    return interes 
+ }
+ else if  ( cuotas == 9){
+    let interes = monto * 0.50;
+    return interes 
+ }
+ else if ( cuotas == 12){
+    let interes = monto * 0.80;
+    return interes
+ }
+ else if (cuotas == 24){
+    let interes = monto * 100;
+    return interes 
+ }
+
+}
+
+console.log ("Pediste:  ", monto);
+console.log ("Cuotas: ", cuotas);
+console.log ("Total con intereses: ", monto + intereses_cuotas (monto , cuotas) );
