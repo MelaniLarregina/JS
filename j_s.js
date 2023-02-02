21f659baa28c548c62f3aa9672fe96a42cda9227
MelaniLarregina <118478149+MelaniLarregina@users.noreply.github.com>
2023-02-02 16:17:26 -0300

    esta es mi version final

--- j_s.js
+++ j_s.js
@@ -1,24 +1,33 @@
-
-console.log ("Bienvenidos a tu prestamo online inmediato");
+alert ("Bienvenidos a tu prestamo online inmediato");
 let nombre = prompt ("Ingresa tu nombre y apellido");
 let dni = prompt("Ingrese su documento");
 let edad= prompt("Ingrese su edad");
 
+if(mayor_de_edad(edad)){
+   let monto = prompt ("Ingresa cuanto dinero quieres solicitar:  ");
+   monto = parseInt (monto);
+
+   let cuotas = prompt ("En cuantas cuotas: 6 , 9 , 12 , 24");
+
+   let total = monto + intereses_cuotas (monto , cuotas);
+   let total_con_iva = total + iva_prestamo(total);
+
+   alert("Pediste:  " + monto);
+   alert("Cuotas: " + cuotas);
+   alert("Total con intereses: " + total);
+   alert("Total con intereses e iva: " + total_con_iva);
+}
+
 function mayor_de_edad (edad){
-
-    if( edad >=18 && typeof (edad) == "number"){
-    console.log ("Correcto, es mayor de edad");
+   if( edad >=18 ){
+      alert ("Correcto, es mayor de edad");
+      return true;
+   }
+   else {
+      alert ("Error, no es mayor de edad");
+      return false;
+   }
 }
-
-    else {console.log ("Error, no mayor de edad");
-
-    }
-}
-
-let monto = prompt ("Ingresa cuanto dinero quieres solicitar:  ");
-monto = parseInt (monto);
-
-let cuotas = prompt ("En cuantas cuotas: 6 , 9 , 12 , 24");
 
 function intereses_cuotas ( monto , cuotas){
    var interes = 0;
@@ -40,14 +49,3 @@ function intereses_cuotas ( monto , cuotas){
 function iva_prestamo ( total ){
    return total * 0.21;
 }
-
-let total = monto + intereses_cuotas (monto , cuotas);
-let total_con_iva = total + iva_prestamo(total);
-
-alert("Pediste:  " + monto);
-alert("Cuotas: " + cuotas);
-alert("Total con intereses: " + total);
-alert("Total con intereses e iva: " + total_con_iva);
-
-
-
